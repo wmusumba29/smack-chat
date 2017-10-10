c6e79012daf6b2e5bc6a0d9daeb8e47efb310d94
William Musumba <william@Williams-MacBook-Pro.local>
2017-10-10 21:06:40 +0100

    first push

---
+++ Smack/Utilies/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by William Musumba on 07/10/2017.
+//  Copyright © 2017 William Musumba. All rights reserved.
+//
+
+import Foundation
---
+++ Smack/View/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by William Musumba on 07/10/2017.
+//  Copyright © 2017 William Musumba. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
